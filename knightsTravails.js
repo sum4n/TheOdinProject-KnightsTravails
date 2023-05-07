9094233ce828abc48569365438855181dd8abd59
Suman Das <sum4ndas@gmail.com>
2023-05-07 23:31:42 +0530

    Fix marking gameBoard with <= 7

--- knightsTravails.js
+++ knightsTravails.js
@@ -28,7 +28,7 @@ function possibleMoves(x, y) {
     gameBoard[x - 2][y - 1] = 1;
     possibleMoves(x - 2, y - 1);
   }
-  if (x - 2 >= 0 && y + 1 < 7 && gameBoard[x - 2][y + 1] == 0) {
+  if (x - 2 >= 0 && y + 1 <= 7 && gameBoard[x - 2][y + 1] == 0) {
     console.log([x - 2], [y + 1]);
     gameBoard[x - 2][y + 1] = 1;
     possibleMoves(x - 2, y + 1);
@@ -38,27 +38,27 @@ function possibleMoves(x, y) {
     gameBoard[x - 1][y - 2] = 1;
     possibleMoves(x - 1, y - 2);
   }
-  if (x - 1 >= 0 && y + 2 < 7 && gameBoard[x - 1][y + 2] == 0) {
+  if (x - 1 >= 0 && y + 2 <= 7 && gameBoard[x - 1][y + 2] == 0) {
     console.log([x - 1], [y + 2]);
     gameBoard[x - 1][y + 2] = 1;
     possibleMoves(x - 1, y + 2);
   }
-  if (x + 1 < 7 && y - 2 >= 0 && gameBoard[x + 1][y - 2] == 0) {
+  if (x + 1 <= 7 && y - 2 >= 0 && gameBoard[x + 1][y - 2] == 0) {
     console.log([x + 1], [y - 2]);
     gameBoard[x + 1][y - 2] = 1;
     possibleMoves(x + 1, y - 2);
   }
-  if (x + 1 < 7 && y + 1 < 7 && gameBoard[x + 1][y + 1] == 0) {
+  if (x + 1 <= 7 && y + 1 <= 7 && gameBoard[x + 1][y + 1] == 0) {
     console.log([x + 1], [y + 2]);
     gameBoard[x + 1][y + 1] = 1;
     possibleMoves(x + 1, y + 2);
   }
-  if (x + 2 < 7 && y - 1 >= 0 && gameBoard[x + 2][y - 1] == 0) {
+  if (x + 2 <= 7 && y - 1 >= 0 && gameBoard[x + 2][y - 1] == 0) {
     console.log([x + 2], [y - 1]);
     gameBoard[x + 2][y - 1] = 1;
     possibleMoves(x + 2, y - 1);
   }
-  if (x + 2 < 7 && y + 1 < 7 && gameBoard[x + 2][y + 1] == 0) {
+  if (x + 2 <= 7 && y + 1 <= 7 && gameBoard[x + 2][y + 1] == 0) {
     console.log([x + 2], [y + 1]);
     gameBoard[x + 2][y + 1] = 1;
     possibleMoves(x + 2, y + 1);
@@ -66,4 +66,4 @@ function possibleMoves(x, y) {
 }
 
 possibleMoves(3, 3);
-console.log(gameBoard);
+// console.log(gameBoard);
