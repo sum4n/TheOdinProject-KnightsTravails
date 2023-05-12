97cbc24c27bd724b87c5b5dda3a6155bfe0135da
Suman Das <sum4ndas@gmail.com>
2023-05-12 18:59:02 +0530

    Get the shortest node route to target

--- knightsTravails.js
+++ knightsTravails.js
@@ -11,19 +11,31 @@ let gameBoard = [
 
 // console.log(gameBoard[1][2]);
 
+class Node {
+  constructor(parent, data) {
+    this.parent = parent;
+    this.name = data;
+  }
+}
+
 class Knight {
-  constructor(arr) {
+  constructor(arr, parent = null) {
     gameBoard[arr[0]][arr[1]] = 1;
     this.position = arr;
+    this.parent = new Node(parent, arr);
     // just one branch of next position
     this.next = [];
+  }
 
+  move(arr = this.position) {
     if (
       arr[0] - 2 >= 0 &&
       arr[1] - 1 >= 0 &&
       gameBoard[arr[0] - 2][arr[1] - 1] == 0
     ) {
-      this.next.push([arr[0] - 2, arr[1] - 1]);
+      // let node = new Knight([arr[0] - 2, arr[1] - 1], arr);
+      this.next.push([this.parent, [arr[0] - 2, arr[1] - 1]]);
+      // this.next.push(node);
       gameBoard[arr[0] - 2][arr[1] - 1] = 1;
     }
     if (
@@ -31,7 +43,9 @@ class Knight {
       arr[1] + 1 <= 7 &&
       gameBoard[arr[0] - 2][arr[1] + 1] == 0
     ) {
-      this.next.push([arr[0] - 2, arr[1] + 1]);
+      this.next.push([this.parent, [arr[0] - 2, arr[1] + 1]]);
+      // let node = new Knight([arr[0] - 2, arr[1] + 1], arr);
+      // this.next.push(node);
       gameBoard[arr[0] - 2][arr[1] + 1] = 1;
     }
     if (
@@ -39,7 +53,9 @@ class Knight {
       arr[1] - 2 >= 0 &&
       gameBoard[arr[0] - 1][arr[1] - 2] == 0
     ) {
-      this.next.push([arr[0] - 1, arr[1] - 2]);
+      // let node = new Knight([arr[0] - 1, arr[1] - 2], arr);
+      // this.next.push(node);
+      this.next.push([this.parent, [arr[0] - 1, arr[1] - 2]]);
       gameBoard[arr[0] - 1][arr[1] - 2] = 1;
     }
     if (
@@ -47,7 +63,9 @@ class Knight {
       arr[1] + 2 <= 7 &&
       gameBoard[arr[0] - 1][arr[1] + 2] == 0
     ) {
-      this.next.push([arr[0] - 1, arr[1] + 2]);
+      // let node = new Knight([arr[0] - 1, arr[1] + 2], arr);
+      // this.next.push(node);
+      this.next.push([this.parent, [arr[0] - 1, arr[1] + 2]]);
       gameBoard[arr[0] - 1][arr[1] + 2] = 1;
     }
     if (
@@ -55,7 +73,9 @@ class Knight {
       arr[1] - 2 >= 0 &&
       gameBoard[arr[0] + 1][arr[1] - 2] == 0
     ) {
-      this.next.push([arr[0] + 1, arr[1] - 2]);
+      // let node = new Knight([arr[0] + 1, arr[1] - 2], arr);
+      // this.next.push(node);
+      this.next.push([this.parent, [arr[0] + 1, arr[1] - 2]]);
       gameBoard[arr[0] + 1][arr[1] - 2] = 1;
     }
     if (
@@ -63,7 +83,9 @@ class Knight {
       arr[1] + 2 <= 7 &&
       gameBoard[arr[0] + 1][arr[1] + 2] == 0
     ) {
-      this.next.push([arr[0] + 1, arr[1] + 2]);
+      // let node = new Knight([arr[0] + 1, arr[1] + 2], arr);
+      // this.next.push(node);
+      this.next.push([this.parent, [arr[0] + 1, arr[1] + 2]]);
       gameBoard[arr[0] + 1][arr[1] + 2] = 1;
     }
     if (
@@ -71,7 +93,9 @@ class Knight {
       arr[1] - 1 >= 0 &&
       gameBoard[arr[0] + 2][arr[1] - 1] == 0
     ) {
-      this.next.push([arr[0] + 2, arr[1] - 1]);
+      // let node = new Knight([arr[0] + 2, arr[1] - 1], arr);
+      // this.next.push(node);
+      this.next.push([this.parent, [arr[0] + 2, arr[1] - 1]]);
       gameBoard[arr[0] + 2][arr[1] - 1] = 1;
     }
     if (
@@ -79,30 +103,59 @@ class Knight {
       arr[1] + 1 <= 7 &&
       gameBoard[arr[0] + 2][arr[1] + 1] == 0
     ) {
-      this.next.push([arr[0] + 2, arr[1] + 1]);
+      // let node = new Knight([arr[0] + 2, arr[1] + 1], arr);
+      // this.next.push(node);
+      this.next.push([this.parent, [arr[0] + 2, arr[1] + 1]]);
       gameBoard[arr[0] + 2][arr[1] + 1] = 1;
     }
   }
 }
 
-class travelTree {
-  constructor(arr) {
-    this.root = this.buildTree(arr);
-  }
+// let node = new Node(null, [3, 3]);
+// console.log(node);
 
-  buildTree(array) {
-    let knight = new Knight(array);
+// let knight = new Knight(node.name);
+// knight.move();
+// let array = knight.next;
 
-    console.log(knight);
-    console.log(knight.position);
+// using queue to travel
+function travel(node, array, result) {
+  if (array.length != 0) {
+    if (node.name[0] == result[0] && node.name[1] == result[1]) {
+      // console.log(node);
+      return node;
+    }
+    let node1 = new Node(array[0][0], array[0][1]);
+    // console.log(node1);
 
-    // if (knight.position[0] == 0 && (knight.position[1] = 4)) return knight;
+    let knight1 = new Knight(node1.name, node1.parent);
+    knight1.move();
+    // console.log(knight1.next);
+    // console.log();
 
-    return knight;
+    array.push(...knight1.next);
+
+    array.shift();
+
+    return travel(node1, array, result);
   }
 }
 
-let tree = new travelTree([3, 3]);
-console.log();
-console.log(tree.root);
-console.table(gameBoard);
+// let result = travel(node, array);
+// console.log(result);
+
+// console.table(gameBoard);
+
+function knightMoves(inputArray, resultArray) {
+  let node = new Node(null, inputArray);
+
+  let knight = new Knight(node.name);
+  knight.move();
+  let array = knight.next;
+
+  let targetNode = travel(node, array, resultArray);
+
+  console.log(targetNode);
+}
+
+knightMoves([3, 3], [4, 3]);
